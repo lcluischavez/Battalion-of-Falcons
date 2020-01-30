7cfd66548e7b5fa6f1b228c8eaa043bba0b46193
Case Scally <casescally@gmail.com>
2020-01-30 15:16:24 -0600

    checking out master

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Route } from "react-router-dom"
 
-
 // import MessageList from "./message/MessageList"
 // import MessageDetails from "./message/MessageDetails"
 import { MessageProvider } from "./message/MessageProvider"
