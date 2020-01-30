727bd6f69280b28fe35edd07369a713070201cdc
Mitali Vasisht <vamdev37@gmail.com>
2020-01-30 14:19:21 -0600

    new master

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,21 +1,22 @@
+import React from "react"
+import { Route } from "react-router-dom"
 import { FriendProvider } from "./friend/FriendProvider"
 import FriendList from "./friend/FriendList"
-import FriendForm from "./friend/FriendForm"
-
+import FriendForm from "./fried/FriendForm"
 
 export default (props) => {
     return (
         <>
             <FriendProvider>
-            <FriendForm>
-                <Route exact path="/">
-                    <FriendList />
-                </Route>
-                <Route exact path="/friends/create">
+                    <Route exact path="/">
+                        <FriendList />
+                    </Route>
+                    <Route exact path="/friends/create">
+                    props => <FriendForm {...props} />
                         <FriendForm />
                     </Route>
-                    </FriendForm>
+               
             </FriendProvider>
-            </>
+        </>
     )
 }
