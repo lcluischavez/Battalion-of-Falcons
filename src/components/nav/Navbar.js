6fffdc695adf3d702bc044369bad59989768eedf
Case Scally <casescally@gmail.com>
2020-01-30 11:52:27 -0600

    fixed changes

---
+++ src/components/nav/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+
+export default (props) => {
+    return (
+        <ul className="navbar">
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/">Nutshell</Link>
+            </li>
+
+            {
+                localStorage.getItem("currentUser")
+                    ? <li className="navbar__item">
+                        <Link className="navbar__link"
+                            to=""
+                            onClick={e => {
+                                e.preventDefault()
+                                localStorage.removeItem("currentUser")
+                                props.history.push("/")
+                            }}
+                        >Logout</Link>
+                    </li>
+                    : ""
+
+            }
+        </ul>
+    )
+}
