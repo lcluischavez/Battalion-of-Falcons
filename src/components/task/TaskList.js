aea559c11ffeff17ea80389875129fb1c135e744
Case Scally <casescally@gmail.com>
2020-01-31 15:36:26 -0600

    tasks working except for delete or edit

--- src/components/task/TaskList.js
+++ src/components/task/TaskList.js
@@ -6,16 +6,41 @@ import "./Tasks.css"
 export default (props) => {
 
     const { tasks } = useContext(TaskContext)
+    const userTasks = tasks.filter(task => task.userId === parseInt(localStorage.getItem("currentUser")))
+    const doneTask = userTasks.filter(task => task.done === true)
+    const openTask = userTasks.filter(task => task.done === false)
+
+    console.log(userTasks)
 
     return (
         <div className="tasks">
+
             <h1>Tasks</h1>
+
             <button onClick={() => props.history.push("/tasks/create")}>
                 Add Task
             </button>
+
+            <h2>Done</h2>
+            <article className="taskList">
+                {
+                    doneTask.map(tas => <Task key={tas.id} task={tas} {...props} />)
+                }
+            </article>
+
+            <h2>Open</h2>
+            <article className="taskList">
+                {
+                    openTask.map(tas => <Task key={tas.id} task={tas} {...props} />)
+                }
+            </article>
+
             <article className="taskList">
                 {tasks.map(task => <Task key={task.id} task={task} />)}
             </article>
+
         </div>
+
+        
     )
 }
