05cdd4062687d156bfaf9d359859458ea7809215
Mitali Vasisht <vamdev37@gmail.com>
2020-01-31 09:59:27 -0600

    change code

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,19 +2,45 @@ import React from "react"
 import { Route } from "react-router-dom"
 
 
-// import MessageList from "./message/MessageList"
-// import MessageDetails from "./message/MessageDetails"
+import MessageList from "./message/MessageList"
+import MessageDetails from "./message/MessageDetails"
 import { MessageProvider } from "./message/MessageProvider"
 import MessageForm from "./message/MessageForm"
+import { FriendProvider } from "./friends/FriendProvider"
+import FriendForm from "./friends/FriendForm"
+import FriendDetails from "./friends/FriendDetails"
+import FriendList from "./friends/FriendList"
+
+
 
 export default (props) => {
     return (
         <>
+            
+           
             <MessageProvider>
+                <Route exact path="/messages" render={
+                    props => <MessageList {...props} />
+                } />
                 <Route exact path="/messages/create">
                     <MessageForm />
                 </Route>
+                <Route path="/messages/:messageId(\d+)" render={
+                    props => <MessageDetails {...props} />
+                } />
             </MessageProvider>
+
+            <FriendProvider>
+                <Route exact path="/friends" render={
+                    props => <FriendList {...props} />
+                } />
+                <Route exact path="/friends/create">
+                    <FriendForm />
+                </Route>
+                <Route path="/friends/:friendId(\d+)" render={
+                    props => <FriendDetails {...props} />
+                } />
+            </FriendProvider>
         </>
     )
 }
---
+++ src/components/friends/FriendProvider.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react"
+
+export const FriendContext = React.createContext()
+
+export const FriendProvider = (props) => {
+    const [friend, changeFriendState] = useState([])
+
+    const getFriends = () => {
+        return fetch("http://localhost:8088/friends")
+            .then(res => res.json())
+            .then(changeFriendState)
+    }
+
+    const addFriend = friend => {
+        return fetch("http://localhost:8088/friends", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(friend)
+        })
+            .then(getFriends)
+    }
+
+    const deleteFriend = friend => {
+        return fetch(`http://localhost:8088/friends/${friend.id}`, {
+            method: "DELETE"
+        })
+        .then(getFriends)
+    }
+
+    const updateFriend = friend => {
+        return fetch(`http://localhost:8088/friends/${friend.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(friend)
+        })
+            .then(getFriends)
+    }
+
+    useEffect(() => {
+        getFriends()
+    }, [])
+
+    useEffect(() => {
+        console.log(friend)
+    }, [friend])
+
+    return (
+        <FriendContext.Provider value={{
+            friend, addFriend, deleteFriend, updateFriend
+        }}>
+            {props.children}
+        </FriendContext.Provider>
+    )
+}
