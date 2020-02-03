6af753467b960a5cfee593a933396fbcd451837f
philipDmartin <philipnathan788@gmail.com>
2020-02-03 10:15:28 -0600

    checking case code

--- src/components/message/MessageForm.js
+++ src/components/message/MessageForm.js
@@ -1,60 +1,3 @@
-// import React, { useContext, useRef } from "react"
-// import { MessageContext } from "./MessageProvider"
-// import "./Messages.css"
-// import { useState } from "react"
-
-// export default props => {
-//     const { addMessage } = useContext(MessageContext)
-//     const [MessageName, changeMessageNameStateTo] = useState("")
-//     const messageMessage = useRef(null)
-
-//     const onUserTypingSomething = e => {
-//         changeMessageNameStateTo(e.target.value)
-//     }
-    
-
-//     const constructNewMessage = () => {
-//         addMessage({
-//             message: messageMessage.current.value
-//         })
-//         .then(() => {
-//             changeMessageNameStateTo("")
-//         })
-//     }
-
-//     return (
-//         <form className="messageForm">
-//             <h2 className="messageForm__title">New Message</h2>
-//             <fieldset>
-//                 <div className="form-group">
-//                     <label htmlFor="messages">Message: </label>
-//                     <input
-//                         type="text"
-//                         id="messages"
-//                         ref={messageMessage}
-//                         value={messageMessage}
-//                         onChange={onUserTypingSomething}
-//                         required
-//                         autoFocus
-//                         className="form-control"
-//                         placeholder="Message"
-//                     />
-//                 </div>
-//             </fieldset>
-//             <button type="submit"
-//                 onClick={
-//                     evt => {
-//                         evt.preventDefault()
-//                         constructNewMessage()
-//                     }
-//                 }
-//                 className="btn btn-primary">
-//                 Save Message
-//             </button>
-//         </form>
-//     )
-// }
-
 import React, { useContext, useRef } from "react"
 import { useState, useEffect } from "react"
 import { MessageContext } from "./MessageProvider"
@@ -67,11 +10,9 @@ export default props => {
     const constructNewMessage = () => {
             addMessage({
                 message: messageMessage.current.value,                
-
             })
         }
     
-
     return (
         <form className="eventForm">
             <h2 className="eventForm__title">New Message</h2>
@@ -100,12 +41,3 @@ export default props => {
         </form>
     )
 }
-
-
-
-
-
-
-
-
-
