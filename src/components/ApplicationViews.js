c60b07550511831f701cc03d96e4437fcc50e76b
Case Scally <casescally@gmail.com>
2020-02-03 14:37:41 -0600

    fixed task edit

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -77,7 +77,6 @@ export default props => {
                     </ArticleProvider>
                 </EventProvider>
             </TaskProvider>
-
         </>
     );
 };
--- src/components/friends/FriendList.js
+++ src/components/friends/FriendList.js
@@ -44,6 +44,10 @@ export default (props) => {
 
             </article>
 
+            <article>
+                Freind Requests:
+            </article>
+
         </div>
     )
 }
